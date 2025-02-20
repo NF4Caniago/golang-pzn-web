d239ff175a180d66ab24d4960be69f30fa6e6b38
NF4Caniago <afifilhamcaniago@gmail.com>
2025-02-20 23:43:42 +0700

    serve file

---
+++ serve_file_test.go
@@ -0,0 +1,54 @@
+package golangpznweb
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		http.ServeFile(w, r, "./resources/ok.html")
+	} else {
+		http.ServeFile(w, r, "./resources/no.html")
+	}
+}
+
+func TestServeFileServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/ok.html
+var resourceOk string
+
+//go:embed resources/no.html
+var resourceNo string
+
+func ServeFileEmbed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		fmt.Fprint(w, resourceOk)
+	} else {
+		fmt.Fprint(w, resourceNo)
+	}
+}
+
+func TestServeFileServerEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
